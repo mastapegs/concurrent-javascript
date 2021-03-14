e2155ddb55967181e08125fe473a4518e3748e96
Matthew Pagan <mastapegs01@gmail.com>
2021-03-14 17:51:28 -0400

    copy over callback code to promises

--- src/promises/index.js
+++ src/promises/index.js
@@ -1,31 +1,53 @@
-fetch('https://swapi.dev/api/starships/')
-  .then(res => res.json())
-  .then(starshipsData => {
-    const starshipsArray = starshipsData.results
-    console.log(starshipsArray)
-    const starshipsDiv = document.getElementById('starships')
-    starshipsDiv.innerHTML = `
-      <table>
-        <thead>
+const starshipsRequest = new XMLHttpRequest()
+starshipsRequest.addEventListener('load', function () {
+  const starshipResponse = JSON.parse(this.response)
+  const starshipsArray = starshipResponse.results
+  const starshipsDiv = document.getElementById('starships')
+  starshipsDiv.innerHTML = `
+    <table>
+      <thead>
+        <tr>
+          <th>Starship Name</th>
+          <th>Model</th>
+          <th>Manufacturer</th>
+          <th>Cost in Credits</th>
+          <th>Crew</th>
+          <th>Appeared In</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${starshipsArray.map(starship => `
           <tr>
-            <th>Starship Name</th>
-            <th>Model</th>
-            <th>Manufacturer</th>
-            <th>Cost in Credits</th>
-            <th>Crew</th>
+            <td>${starship.name}</td>
+            <td>${starship.model}</td>
+            <td>${starship.manufacturer}</td>
+            <td>${starship.cost_in_credits}</td>
+            <td>${starship.crew}</td>
+            <td data-movies=${JSON.stringify(starship.films)} class="appearedIn"></td>
           </tr>
-        </thead>
-        <tbody>
-          ${starshipsArray.map(starship => `
-            <tr>
-              <td>${starship.name}</td>
-              <td>${starship.model}</td>
-              <td>${starship.manufacturer}</td>
-              <td>${starship.cost_in_credits}</td>
-              <td>${starship.crew}</td>
-            </tr>
-          `).join('')}
-        </tbody>
-      </table>
-    `
+        `).join('')}
+      </tbody>
+    </table>
+  `
+  const appearedInArray = document.querySelectorAll('.appearedIn')
+  appearedInArray.forEach(starshipTD => {
+    const movieURLs = JSON.parse(starshipTD.dataset.movies)
+    const movieNames = []
+    let movieNamesCompletedRequests = 0
+    for (let i = 0; i < movieURLs.length; i++) {
+      const movieNameRequest = new XMLHttpRequest()
+      movieNameRequest.addEventListener('load', function() {
+        const movieNameResponse = JSON.parse(this.response)
+        movieNames.push(movieNameResponse.title)
+        movieNamesCompletedRequests++
+        if (movieNamesCompletedRequests === movieURLs.length) {
+          starshipTD.innerHTML = movieNames.join()
+        }
+      })
+      movieNameRequest.open('GET', movieURLs[i])
+      movieNameRequest.send()
+    }
   })
+})
+starshipsRequest.open('GET', 'https://swapi.dev/api/starships/')
+starshipsRequest.send()
