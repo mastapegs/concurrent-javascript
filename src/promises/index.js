74548a4ecc09a87722878aeb94fbed0da4ade7fc
Matthew Pagan <mastapegs01@gmail.com>
2021-03-14 18:09:25 -0400

    implement solution with promises

--- src/promises/index.js
+++ src/promises/index.js
@@ -1,53 +1,43 @@
-const starshipsRequest = new XMLHttpRequest()
-starshipsRequest.addEventListener('load', function () {
-  const starshipResponse = JSON.parse(this.response)
-  const starshipsArray = starshipResponse.results
-  const starshipsDiv = document.getElementById('starships')
-  starshipsDiv.innerHTML = `
-    <table>
-      <thead>
-        <tr>
-          <th>Starship Name</th>
-          <th>Model</th>
-          <th>Manufacturer</th>
-          <th>Cost in Credits</th>
-          <th>Crew</th>
-          <th>Appeared In</th>
-        </tr>
-      </thead>
-      <tbody>
-        ${starshipsArray.map(starship => `
+fetch('https://swapi.dev/api/starships/')
+  .then(starshipsResponse => starshipsResponse.json())
+  .then(starshipsData => {
+    const starshipsArray = starshipsData.results
+    const starshipsDiv = document.getElementById('starships')
+    starshipsDiv.innerHTML = `
+      <table>
+        <thead>
           <tr>
-            <td>${starship.name}</td>
-            <td>${starship.model}</td>
-            <td>${starship.manufacturer}</td>
-            <td>${starship.cost_in_credits}</td>
-            <td>${starship.crew}</td>
-            <td data-movies=${JSON.stringify(starship.films)} class="appearedIn"></td>
+            <th>Starship Name</th>
+            <th>Model</th>
+            <th>Manufacturer</th>
+            <th>Cost in Credits</th>
+            <th>Crew</th>
+            <th>Appeared In</th>
           </tr>
-        `).join('')}
-      </tbody>
-    </table>
-  `
-  const appearedInArray = document.querySelectorAll('.appearedIn')
-  appearedInArray.forEach(starshipTD => {
-    const movieURLs = JSON.parse(starshipTD.dataset.movies)
-    const movieNames = []
-    let movieNamesCompletedRequests = 0
-    for (let i = 0; i < movieURLs.length; i++) {
-      const movieNameRequest = new XMLHttpRequest()
-      movieNameRequest.addEventListener('load', function() {
-        const movieNameResponse = JSON.parse(this.response)
-        movieNames.push(movieNameResponse.title)
-        movieNamesCompletedRequests++
-        if (movieNamesCompletedRequests === movieURLs.length) {
-          starshipTD.innerHTML = movieNames.join()
-        }
+        </thead>
+        <tbody>
+          ${starshipsArray.map(starship => `
+            <tr>
+              <td>${starship.name}</td>
+              <td>${starship.model}</td>
+              <td>${starship.manufacturer}</td>
+              <td>${starship.cost_in_credits}</td>
+              <td>${starship.crew}</td>
+              <td data-movies=${JSON.stringify(starship.films)} class="appearedIn"></td>
+            </tr>
+          `).join('')}
+        </tbody>
+      </table>
+    `
+    const appearedInTDArray = document.querySelectorAll('.appearedIn')
+    appearedInTDArray.forEach(starshipTD => {
+      const movieURLs = JSON.parse(starshipTD.dataset.movies)
+      const movieNamePromises = movieURLs.map(movieURL => {
+        return fetch(movieURL).then(res => res.json()).then(movieData => movieData.title)
       })
-      movieNameRequest.open('GET', movieURLs[i])
-      movieNameRequest.send()
-    }
+      Promise.all(movieNamePromises)
+        .then(movieNames => {
+          starshipTD.innerHTML = movieNames.join()
+        })
+    })
   })
-})
-starshipsRequest.open('GET', 'https://swapi.dev/api/starships/')
-starshipsRequest.send()
